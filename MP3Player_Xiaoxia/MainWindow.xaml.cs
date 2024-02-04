a036a87df5d26f64bc14df53abda62a02ca08965
XiaoxiaNSCC <w0471965@nscc.ca>
2024-02-04 14:17:52 -0400

    Read tags works

--- MP3Player_Xiaoxia/MainWindow.xaml.cs
+++ MP3Player_Xiaoxia/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,12 +27,19 @@ namespace MP3Player_Xiaoxia
         private bool isPlaying=false;
         private bool isUserDraggingSlider=false;
         
+        
         private readonly DispatcherTimer timer = new() { Interval = TimeSpan.FromSeconds(0.1) };
+        private readonly OpenFileDialog MediaOpenDialog = new()
+        {
+            Title = "Pick a MP3 file",
+            Filter = "Media Files (*.mp3,*.mp4)|*.mp3;*.mp4"
+        };
         public MainWindow()
         {
             InitializeComponent();
 
             timer.Tick += Timer_Tick;
+            timer.Start();
         }
         //update slider value by seconds, if file been loaded, the duration of the file is awailable and the user isn't ragging he slider.
         private void Timer_Tick(object? sender, EventArgs e)
@@ -50,31 +58,32 @@ namespace MP3Player_Xiaoxia
 
         private void BtnFile_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Filter = "MP3 Source Files | *.mp3";
-            fileDialog.Title = "Pick a MP3 file";
-            fileDialog.Multiselect = false;
-            bool? success = fileDialog.ShowDialog();
+
+            MediaOpenDialog.Multiselect = false;
+            bool? success = MediaOpenDialog.ShowDialog();
             if(success == true) 
             {
-                string path = fileDialog.FileName;
-                fileName = fileDialog.SafeFileName;
+                string path = MediaOpenDialog.FileName;
+                fileName = MediaOpenDialog.SafeFileName;
                 LblTest.Content = fileName;
                 Player.Source = new Uri(path);
                 BtnPlay.IsEnabled = true;
+                Player.Play();
+                Show_Properties(sender, e);
             }
             else
             {
                 //didn't pick a file
             }
         }
-
+        #region Media Controls
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
             if(Player?.Source != null)
             {
                 Player.Play();
                 isPlaying = true;
+                //need a default value for label
             }
 
         }
@@ -95,6 +104,7 @@ namespace MP3Player_Xiaoxia
         {
                 isUserDraggingSlider = true;                
         }
+        //why this method doesn't work
         private void ProgressSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
         {
             isUserDraggingSlider = false;
@@ -103,11 +113,69 @@ namespace MP3Player_Xiaoxia
         private void ProgressSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             LblTime.Content = TimeSpan.FromSeconds(ProgressSlider.Value).ToString(@"hh\:mm\:ss");
+       
+        }
+
+        private void ProgressSlider_DragCompleted_1(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            isUserDraggingSlider = false;
             Player.Position = TimeSpan.FromSeconds(ProgressSlider.Value);
         }
 
         //Play and pause funcion
+        #endregion
+        #region Properties
+        private void Show_Properties(object sender, RoutedEventArgs e)
+        {
+            //string MP3_Title = 
+            var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
+            StringBuilder sb = new();
+            var tags = tfile.Tag.Title.ToString();
+            
+            sb.AppendLine(tags);    
 
+            sb.AppendLine("Duration: " + tfile.Properties.Duration.ToString(@"hh\:mm\:ss"));
+
+            if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Audio))
+            {
+                sb.AppendLine("Audio bitrate: " + tfile.Properties.AudioBitrate);
+                sb.AppendLine("Audio sample rate: " + tfile.Properties.AudioSampleRate);
+                sb.AppendLine("Audio channels: " + (tfile.Properties.AudioChannels == 1 ? "Mono" : "Stereo"));
+            }
+
+            if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Video))
+            {
+                sb.AppendLine($"Video resolution: {tfile.Properties.VideoWidth} x {tfile.Properties.VideoHeight}");
+            }
+            TextBlockTags.Text = sb.ToString();
+        }
+
+        private void PropertiesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MediaOpenDialog.FileName != "")
+            {
+                var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
+                StringBuilder sb = new();
+
+                sb.AppendLine("Duration: " + tfile.Properties.Duration.ToString(@"hh\:mm\:ss"));
+
+                if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Audio))
+                {
+                    sb.AppendLine("Audio bitrate: " + tfile.Properties.AudioBitrate);
+                    sb.AppendLine("Audio sample rate: " + tfile.Properties.AudioSampleRate);
+                    sb.AppendLine("Audio channels: " + (tfile.Properties.AudioChannels == 1 ? "Mono" : "Stereo"));
+                }
+
+                if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Video))
+                {
+                    sb.AppendLine($"Video resolution: {tfile.Properties.VideoWidth} x {tfile.Properties.VideoHeight}");
+                }
+
+                MessageBox.Show(sb.ToString(), "Properties");
+            }
+        }
+
+        #endregion
 
     }
 }
