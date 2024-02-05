13a6f0f25ff3f5c6b09b63aa663257529984c7a5
XiaoxiaNSCC <w0471965@nscc.ca>
2024-02-05 18:28:17 -0400

    Switch window works

--- MP3Player_Xiaoxia/MainWindow.xaml.cs
+++ MP3Player_Xiaoxia/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,8 +26,10 @@ namespace MP3Player_Xiaoxia
     public partial class MainWindow : Window
     {
         string? fileName;
+        string? filePath;
         private bool isPlaying=false;
         private bool isUserDraggingSlider=false;
+        private NowPlaying nowPlaying;
                 
         private readonly DispatcherTimer timer = new() { Interval = TimeSpan.FromSeconds(0.1) };
         private readonly OpenFileDialog MediaOpenDialog = new()
@@ -36,22 +40,17 @@ namespace MP3Player_Xiaoxia
         public MainWindow()
         {
             InitializeComponent();
-
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
         private void BtnPlay_Click(object sender, RoutedEventArgs e)
         {
-            CC.Content = new NowPlaying();
+            
+            CC.Content = nowPlaying;
+            nowPlaying.Title.TagHeader.Content = "title";
+            //CC.Content = new NowPlaying();
         }
-        //update slider value by seconds, if file been loaded, the duration of the file is awailable and the user isn't ragging he slider.
-        //private void Timer_Tick(object? sender, EventArgs e)
-        //{
-        //    if (Player.Source != null && Player.NaturalDuration.HasTimeSpan && !isUserDraggingSlider)
-        //    {
-        //        ProgressSlider.Maximum = Player.NaturalDuration.TimeSpan.TotalSeconds;
-        //        ProgressSlider.Value = Player.Position.TotalSeconds;
-        //    }
-        //}
 
         private void MenuFile_Click(object sender, RoutedEventArgs e)
         {
@@ -65,8 +64,13 @@ namespace MP3Player_Xiaoxia
             bool? success = MediaOpenDialog.ShowDialog();
             if (success == true)
             {
-                string path = MediaOpenDialog.FileName;
+                filePath = MediaOpenDialog.FileName;
                 fileName = MediaOpenDialog.SafeFileName;
+                Player.Source = new Uri(filePath);
+                Player.Play();
+                nowPlaying = new NowPlaying();
+                CC.Content = nowPlaying;
+                PlayPanel.Visibility = Visibility.Visible;
             }
             else
             {
@@ -80,6 +84,101 @@ namespace MP3Player_Xiaoxia
         }
         //#region Media Controls
 
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (Player.Source != null && Player.NaturalDuration.HasTimeSpan && !isUserDraggingSlider)
+            {
+                ProgressSlider.Maximum = Player.NaturalDuration.TimeSpan.TotalSeconds;
+                ProgressSlider.Value = Player.Position.TotalSeconds;
+            }
+        }
 
+        #region Media Controls
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (Player?.Source != null)
+            {
+                Player.Play();
+                isPlaying = true;
+                //need a default value for label
+            }
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            Player.Pause();
+            isPlaying = false;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            Player.Stop();
+            isPlaying = false;
+        }
+
+        private void ProgressSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            isUserDraggingSlider = true;
+        }
+        //why this method doesn't work
+        private void ProgressSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            isUserDraggingSlider = false;
+            Player.Position = TimeSpan.FromSeconds(ProgressSlider.Value);
+        }
+        private void ProgressSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            LblTime.Content = TimeSpan.FromSeconds(ProgressSlider.Value).ToString(@"hh\:mm\:ss");
+
+        }
+
+        private void ProgressSlider_DragCompleted_1(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            isUserDraggingSlider = false;
+            Player.Position = TimeSpan.FromSeconds(ProgressSlider.Value);
+        }
+
+        //Play and pause funcion
+        #endregion
+
+        private void Show_Properties(object sender, RoutedEventArgs e)
+        {
+            var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
+            var title = tfile.Tag.Title.ToString();
+            var album = tfile.Tag.Album.ToString();
+            var year = tfile.Tag.Year.ToString();
+            var genre = tfile.Tag.Genres.ToString();
+
+
+            //Image image = tfile.Tag.Picture;
+            //tfile.Save(); 
+
+
+        }
+
+        private void PropertiesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MediaOpenDialog.FileName != "")
+            {
+                var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
+                StringBuilder sb = new();
+
+                sb.AppendLine("Duration: " + tfile.Properties.Duration.ToString(@"hh\:mm\:ss"));
+
+                if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Audio))
+                {
+                    sb.AppendLine("Audio bitrate: " + tfile.Properties.AudioBitrate);
+                    sb.AppendLine("Audio sample rate: " + tfile.Properties.AudioSampleRate);
+                    sb.AppendLine("Audio channels: " + (tfile.Properties.AudioChannels == 1 ? "Mono" : "Stereo"));
+                }
+
+                if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Video))
+                {
+                    sb.AppendLine($"Video resolution: {tfile.Properties.VideoWidth} x {tfile.Properties.VideoHeight}");
+                }
+
+                MessageBox.Show(sb.ToString(), "Properties");
+            }
+        }
     }
 }
--- MP3Player_Xiaoxia/NowPlaying.xaml.cs
+++ MP3Player_Xiaoxia/NowPlaying.xaml.cs
@@ -30,114 +30,114 @@ namespace MP3Player_Xiaoxia
         public NowPlaying()
         {
             InitializeComponent();
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            //timer.Tick += Timer_Tick;
+            //timer.Start();
         }
 
 
-        private readonly DispatcherTimer timer = new() { Interval = TimeSpan.FromSeconds(0.1) };
-        private readonly OpenFileDialog MediaOpenDialog = new()
-        {
-            Title = "Pick a MP3 file",
-            Filter = "Media Files (*.mp3,*.mp4)|*.mp3;*.mp4"
-        };
+        //private readonly DispatcherTimer timer = new() { Interval = TimeSpan.FromSeconds(0.1) };
+        //private readonly OpenFileDialog MediaOpenDialog = new()
+        //{
+        //    Title = "Pick a MP3 file",
+        //    Filter = "Media Files (*.mp3,*.mp4)|*.mp3;*.mp4"
+        //};
 
         //update slider value by seconds, if file been loaded, the duration of the file is awailable and the user isn't ragging he slider.
-        private void Timer_Tick(object? sender, EventArgs e)
-        {
-            if (Player.Source != null && Player.NaturalDuration.HasTimeSpan && !isUserDraggingSlider)
-            {
-                ProgressSlider.Maximum = Player.NaturalDuration.TimeSpan.TotalSeconds;
-                ProgressSlider.Value = Player.Position.TotalSeconds;
-            }
-        }
+        //private void Timer_Tick(object? sender, EventArgs e)
+        //{
+        //    if (Player.Source != null && Player.NaturalDuration.HasTimeSpan && !isUserDraggingSlider)
+        //    {
+        //        ProgressSlider.Maximum = Player.NaturalDuration.TimeSpan.TotalSeconds;
+        //        ProgressSlider.Value = Player.Position.TotalSeconds;
+        //    }
+        //}
 
-               #region Media Controls
-        private void btnPlay_Click(object sender, RoutedEventArgs e)
-        {
-            if (Player?.Source != null)
-            {
-                Player.Play();
-                isPlaying = true;
-                //need a default value for label
-            }
-        }
+        //       #region Media Controls
+        //private void btnPlay_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (Player?.Source != null)
+        //    {
+        //        Player.Play();
+        //        isPlaying = true;
+        //        //need a default value for label
+        //    }
+        //}
 
-        private void btnPause_Click(object sender, RoutedEventArgs e)
-        {
-            Player.Pause();
-            isPlaying = false;
-        }
+        //private void btnPause_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Player.Pause();
+        //    isPlaying = false;
+        //}
 
-        private void btnStop_Click(object sender, RoutedEventArgs e)
-        {
-            Player.Stop();
-            isPlaying = false;
-        }
+        //private void btnStop_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Player.Stop();
+        //    isPlaying = false;
+        //}
 
-        private void ProgressSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
-        {
-            isUserDraggingSlider = true;
-        }
-        //why this method doesn't work
-        private void ProgressSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
-        {
-            isUserDraggingSlider = false;
-            Player.Position = TimeSpan.FromSeconds(ProgressSlider.Value);
-        }
-        private void ProgressSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            LblTime.Content = TimeSpan.FromSeconds(ProgressSlider.Value).ToString(@"hh\:mm\:ss");
+        //private void ProgressSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        //{
+        //    isUserDraggingSlider = true;
+        //}
+        ////why this method doesn't work
+        //private void ProgressSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        //{
+        //    isUserDraggingSlider = false;
+        //    Player.Position = TimeSpan.FromSeconds(ProgressSlider.Value);
+        //}
+        //private void ProgressSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    LblTime.Content = TimeSpan.FromSeconds(ProgressSlider.Value).ToString(@"hh\:mm\:ss");
 
-        }
+        //}
 
-        private void ProgressSlider_DragCompleted_1(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
-        {
-            isUserDraggingSlider = false;
-            Player.Position = TimeSpan.FromSeconds(ProgressSlider.Value);
-        }
+        //private void ProgressSlider_DragCompleted_1(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        //{
+        //    isUserDraggingSlider = false;
+        //    Player.Position = TimeSpan.FromSeconds(ProgressSlider.Value);
+        //}
 
-        //Play and pause funcion
-        #endregion
+        ////Play and pause funcion
+        //#endregion
 
-        private void Show_Properties(object sender, RoutedEventArgs e)
-        {
-            var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
-            var title = tfile.Tag.Title.ToString();
-            var album = tfile.Tag.Album.ToString();
-            var year = tfile.Tag.Year.ToString();
-            var genre = tfile.Tag.Genres.ToString();
-
-
-            //Image image = tfile.Tag.Picture;
-            //tfile.Save(); 
+        //private void Show_Properties(object sender, RoutedEventArgs e)
+        //{
+        //    var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
+        //    var title = tfile.Tag.Title.ToString();
+        //    var album = tfile.Tag.Album.ToString();
+        //    var year = tfile.Tag.Year.ToString();
+        //    var genre = tfile.Tag.Genres.ToString();
 
 
-        }
+        //    //Image image = tfile.Tag.Picture;
+        //    //tfile.Save(); 
 
-        private void PropertiesBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (MediaOpenDialog.FileName != "")
-            {
-                var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
-                StringBuilder sb = new();
 
-                sb.AppendLine("Duration: " + tfile.Properties.Duration.ToString(@"hh\:mm\:ss"));
+        //}
 
-                if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Audio))
-                {
-                    sb.AppendLine("Audio bitrate: " + tfile.Properties.AudioBitrate);
-                    sb.AppendLine("Audio sample rate: " + tfile.Properties.AudioSampleRate);
-                    sb.AppendLine("Audio channels: " + (tfile.Properties.AudioChannels == 1 ? "Mono" : "Stereo"));
-                }
+        //private void PropertiesBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (MediaOpenDialog.FileName != "")
+        //    {
+        //        var tfile = TagLib.File.Create(MediaOpenDialog.FileName);
+        //        StringBuilder sb = new();
 
-                if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Video))
-                {
-                    sb.AppendLine($"Video resolution: {tfile.Properties.VideoWidth} x {tfile.Properties.VideoHeight}");
-                }
+        //        sb.AppendLine("Duration: " + tfile.Properties.Duration.ToString(@"hh\:mm\:ss"));
 
-                MessageBox.Show(sb.ToString(), "Properties");
-            }
-        }
+        //        if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Audio))
+        //        {
+        //            sb.AppendLine("Audio bitrate: " + tfile.Properties.AudioBitrate);
+        //            sb.AppendLine("Audio sample rate: " + tfile.Properties.AudioSampleRate);
+        //            sb.AppendLine("Audio channels: " + (tfile.Properties.AudioChannels == 1 ? "Mono" : "Stereo"));
+        //        }
+
+        //        if (tfile.Properties.MediaTypes.HasFlag(TagLib.MediaTypes.Video))
+        //        {
+        //            sb.AppendLine($"Video resolution: {tfile.Properties.VideoWidth} x {tfile.Properties.VideoHeight}");
+        //        }
+
+        //        MessageBox.Show(sb.ToString(), "Properties");
+        //    }
+        //}
     }
 }
